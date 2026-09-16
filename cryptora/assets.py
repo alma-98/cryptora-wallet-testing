8447dc3c1ce602ba858f5d246f62f526708895f6
Alma <125168465+alma-98@users.noreply.github.com>
2026-09-16 11:41:38 +0700

    Build native multi-asset registry

--- cryptora/assets.py
+++ cryptora/assets.py
@@ -1,1 +1,9 @@
-ASSETS = {"ETC": {"name": "EtherCoin", "type": "native_cryptora", "smart_contract": False}}
+ASSETS = {
+    "BTC": {"name": "Bitcoin", "type": "native_cryptora", "smart_contract": False},
+    "USD": {"name": "US Dollar", "type": "native_cryptora", "smart_contract": False},
+    "ETH": {"name": "Ethereum", "type": "native_cryptora", "smart_contract": False},
+    "USDT": {"name": "Tether USD", "type": "native_cryptora", "smart_contract": False},
+    "USDC": {"name": "USD Coin", "type": "native_cryptora", "smart_contract": False},
+    "TRX": {"name": "TRON", "type": "native_cryptora", "smart_contract": False},
+    "ETC": {"name": "EtherCoin", "type": "native_cryptora", "smart_contract": False},
+}
